e88221312b6b19a1f3f811408df48674c27261c2
devious890890 <vmuigaim@gmail.com>
2026-03-01 16:11:45 +0300

    commit 2

---
+++ main.py
@@ -0,0 +1,11 @@
+print("What is The answer to The square root of 625?")
+print("A. 35")
+print("B. 65")
+print("C. 20")
+print("D. 25")
+
+answer = input("Write your here: ")
+
+if answer == ("D"):
+    print("You got it correct")
+else:print("Incorrect answer.")
